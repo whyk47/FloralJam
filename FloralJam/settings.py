d356d29a3401f5d07e46c2b81f92c4935c39e643
Yen Kit Chow <chowyenkit@gmail.com>
2024-09-10 21:57:24 +0800

    delpoyment changes

--- FloralJam/settings.py
+++ FloralJam/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9px4bnc!v5!*zq348bg43lg$lgm^k47ui7xy$@6ns7ge#p*%uw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
