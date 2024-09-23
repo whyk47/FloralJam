6aaf9d5d21a292c0991a1846658f06f2971171fa
Yen Kit Chow <chowyenkit@gmail.com>
2024-09-23 15:30:17 +0800

    migrate to aws RDS

--- FloralJam/settings.py
+++ FloralJam/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'FloralJam.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('RDS_DB'),
+        'USER': os.getenv('RDS_USERNAME'),
+        'PASSWORD': os.getenv('RDS_PW'),
+        'HOST': os.getenv('RDS_ENDPOINT'),
+        'PORT': '3306',
     }
 }
 
