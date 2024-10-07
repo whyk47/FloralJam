2a10fd57832a6077ba00aa661e8a8b828f98f874
Yen Kit Chow <chowyenkit@gmail.com>
2024-10-07 18:57:20 +0800

    .

--- FloralJam/celery.py
+++ FloralJam/celery.py
@@ -7,20 +7,16 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FloralJam.settings')
 
-sqs_queue_url = os.environ.get("SQS_QUEUE_URL")
-aws_access_key = os.environ.get("AWS_ACCESS_KEY")
-aws_secret_key = os.environ.get("AWS_SECRET_KEY")
-
 app = Celery(
     'FloralJam',
-    broker_url=f"sqs://",
+    broker_url="sqs://",
     broker_transport_options={
         "region": "ap-southeast-1",
         "predefined_queues": {
             "celery": {  ## the name of the SQS queue
-                "url": sqs_queue_url,
-                "access_key_id": aws_access_key,
-                "secret_access_key": aws_secret_key,
+                "url": os.environ.get("SQS_QUEUE_URL"),
+                "access_key_id": os.environ.get("AWS_ACCESS_KEY"),
+                "secret_access_key": os.environ.get("AWS_SECRET_KEY"),
             }
         },
     },
